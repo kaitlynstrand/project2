0a28cb3441b24a99e54a9c7d0002d2bbfa8ec738
Kaitlyn <kaitlyn.strand@gmail.com>
2017-11-06 10:51:36 -0600

    monday

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.static("public"));
 require("./routes/group-api-routes.js")(app);
 require("./routes/tasks-api-routes.js")(app);
 require("./routes/user-api-routes.js")(app);
-//require("./routes/html-routes.js")(app);
+require("./routes/html-routes.js")(app);
 
 db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
