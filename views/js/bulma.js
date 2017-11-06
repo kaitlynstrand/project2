c82a8953a241e69872be8614ea4c3668506852fa
Edge Quintanilla <edgarq86@gmail.com>
2017-11-06 15:09:41 -0600

    added minor responsive nav br edits. make your screen small to see

--- views/js/bulma.js
+++ views/js/bulma.js
@@ -1,11 +1,15 @@
 // The following code is based off a toggle menu by @Bradcomp
 // source: https://gist.github.com/Bradcomp/a9ef2ef322a8e8017443b626208999c1
 (function() {
-    var burger = document.querySelector('.nav-toggle');
-    var menu = document.querySelector('.nav-menu');
 
-    burger.addEventListener('click', function() {
-        burger.classList.toggle('is-active');
-        menu.classList.toggle('is-active');
+  console.log('locked and loaded');
+
+    var burger = $('.navbar-burger');
+    var menu = $('#navMenu');
+
+    burger.hover(function() {
+        $('nav').attr('class', 'is-white');
+        burger.attr('class','is-active');
+        menu.attr('class', '');
     });
-});
+})();
