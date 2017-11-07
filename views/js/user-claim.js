502ef28343212053c07baabf0ee17cbe2d63f90e
Kaitlyn <kaitlyn.strand@gmail.com>
2017-11-07 17:07:24 -0600

    new-task click

--- views/js/user-claim.js
+++ views/js/user-claim.js
@@ -1,37 +1,24 @@
-$(document).ready(function() {
+// $(document).ready(function() {
+// 	$(document).on("click", ".button", function(){
+// 		var buttonClicked = $(this).attr("id")
+// 		console.log(buttonClicked)
+// 		$.post("api/task/").then(function(results) {
+// 			console.log(results)
+// 		})
+// 	})
+// })
 
-	var input = $("#user-claim");
 
-//this is where we update the claim to "claimed" and append to 
-//actual users task page "/mytasks" and
-//mark on page with tasks/assignments to "claimed" so no one else can "claim"
-	$(document).on("click", "/")
-})
+// app.put("/api/task/:claim", function(req, res) {
+// 		db.Task.update({
+// 			claim: true
+// 		},
+// 		{
+// 			where: {
+// 				id: req.params.claim
 
-//when current user click "claim", the task, points need to go to their "api/mytasks" page
-//becomes disabled button/"claimed!!" on tasks overall page
-//added onto users "api/mytasks" page
-
-//function 1 - add task to user who claimed it
-
-//function 2 - remove claim availability from open claims list - move to  claimed 
-//but incomplete list
-
-$(document).ready(function() {
-
-	var groupName
-
-	$("#submit-group").on("click", function() {
-		//console.log("clicked")
-		groupName = $("#group-name").val().trim()
-		
-		var newGroupObject = {
-			group_name: groupName
-		}
-			$.post("/api/group", newGroupObject).then(function(results) {
-				console.log(results)
-			})
-		})
-})
-
+// 		}).then(function(results) {
+// 			res.json(results);
+// 		});
+// 	});
 	
--- views/js/user-completed.js
+++ views/js/user-completed.js
@@ -1,44 +1,10 @@
-$(document).ready(function() {
-
-//div id of complete on admin page
-	var input = $("#user-complete");
-
-//this is where we update the task from incomplete to completed
-//remove task from "mytasks" page 
-//change from incomplete to complete on admin page
-//on leaderboard: points of completed are added to user who claimed
-//on leaderboard: points -2 of what were assigned are added to user if claimed involuntarily
-
-
-	$(document).on("click", "/")
-})
-
-//when current user click "claim", the task, points need to go to their "api/mytasks" page
-//becomes disabled button/"claimed!!" on tasks overall page
-//added onto users "api/mytasks" page
-
-//function 1 - add task to user who claimed it
-
-//function 2 - remove claim availability from open claims list - move to  claimed 
-//but incomplete list
-
-$(document).ready(function() {
-
-	var groupName
-
-	$("#submit-group").on("click", function() {
-		//console.log("clicked")
-		groupName = $("#group-name").val().trim()
-		
-		var newGroupObject = {
-			group_name: groupName
-		}
-			$.post("/api/group", newGroupObject).then(function(results) {
-				console.log(results)
-			})
-			return results.render("home", newGroupObject)
-			results.redirect("/")
-		})
-})
-
+// $(document).ready(function() {
 	
+// 	$(document).on("click", "#claim-button", function(){
+// 		var buttonClicked = $(this).val()
+// 		console.log(buttonClicked)
+// 		console.log(this.claim)
+// 	})
+
+// })
+// 	
