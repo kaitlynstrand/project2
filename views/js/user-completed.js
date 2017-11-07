c90b0c8ae2075c7164d8a6ced5d81a67f1711a02
Kaitlyn <kaitlyn.strand@gmail.com>
2017-11-07 10:12:42 -0600

    kait

---
+++ views/js/user-completed.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+//div id of complete on admin page
+	var input = $("#user-complete");
+
+//this is where we update the task from incomplete to completed
+//remove task from "mytasks" page 
+//change from incomplete to complete on admin page
+//on leaderboard: points of completed are added to user who claimed
+//on leaderboard: points -2 of what were assigned are added to user if claimed involuntarily
+
+
+	$(document).on("click", "/")
+})
+
+//when current user click "claim", the task, points need to go to their "api/mytasks" page
+//becomes disabled button/"claimed!!" on tasks overall page
+//added onto users "api/mytasks" page
+
+//function 1 - add task to user who claimed it
+
+//function 2 - remove claim availability from open claims list - move to  claimed 
+//but incomplete list
+
