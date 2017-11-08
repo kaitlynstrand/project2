d35354f229c753fafa5eb17a6f4ffb9ce90f7f5a
Maximillian Teibel <GTCENTRAL@dhcp-10-102-240-94.wireless.northwestern.private>
2017-11-08 09:01:39 -0600

    Add to group

--- views/js/user-claim.js
+++ views/js/user-claim.js
@@ -19,19 +19,7 @@ $(document).ready(function() {
 
 $(document).ready(function() {
 
-	var groupName
 
-	$("#submit-group").on("click", function() {
-		//console.log("clicked")
-		groupName = $("#group-name").val().trim()
-		
-		var newGroupObject = {
-			group_name: groupName
-		}
-			$.post("/api/group", newGroupObject).then(function(results) {
-				console.log(results)
-			})
-		})
 })
 
 	
